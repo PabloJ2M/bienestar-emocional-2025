e881e400994ea267d22178db86f324e195008cd3
Santiago Herrera Panizzo <smhp-1104@hotmail.com>
2025-06-23 22:14:37 -0500

    Cambio de Niveles

--- Assets/Code/Scripts/Manager/StatSender.cs
+++ Assets/Code/Scripts/Manager/StatSender.cs
@@ -8,5 +8,36 @@ public class StatSender : MonoBehaviour
     private GameController _controller;
 
     private void Awake() => _controller = GameController.Instance;
-    public void AddValue() => _controller?.AddAmount(_types, _amount / 100f);
+    public void AddValue()
+    {
+        _controller?.AddAmount(_types, _amount / 100f);
+
+        int nivelActual = PlayerPrefs.GetInt("NivelActual", 0);
+
+        if (_types.HasFlag(StatType.Alimento))
+            IncrementarObjetivo("Comer", nivelActual);
+
+        if (_types.HasFlag(StatType.Limpieza))
+            IncrementarObjetivo("Limpiar", nivelActual);
+
+        if (_types.HasFlag(StatType.Felicidad))
+            IncrementarObjetivo("Jugar", nivelActual);
+
+        if (_types.HasFlag(StatType.Salud))
+            IncrementarObjetivo("Salud", nivelActual);
+
+        PlayerPrefs.Save();
+
+        ObjectivesController controller = FindFirstObjectByType<ObjectivesController>();
+        if (controller != null)
+        {
+            controller.ForzarActualizarTextos();
+        }
+    }
+    private void IncrementarObjetivo(string nombreObjetivo, int nivel)
+    {
+        string key = $"Obj_{nivel}_{nombreObjetivo}";
+        int actual = PlayerPrefs.GetInt(key, 0);
+        PlayerPrefs.SetInt(key, actual + 1);
+    }
 }
